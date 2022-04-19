fd460a02efe41cf889c29e1b20a807921052d2e8
Tran Khanh <63302419+khalongtvh@users.noreply.github.com>
2022-04-19 08:32:23 +0700

    Add project files.

---
+++ bt/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+
+namespace bt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("1.tam giac!");
+            Console.WriteLine("2.tu giac!");
+            Console.WriteLine("3.hinh vuong!");
+            Console.WriteLine("4.hinh binh hanh!");
+            Console.WriteLine("5.hinh chu nhat!");
+            Console.Write("nhap mot trong cac hinh : ");
+            try
+            {
+                int a = int.Parse(Console.ReadLine());
+                switch (a)
+                {
+                    case 1:
+                        tamGiac tg = new tamGiac();
+                        tg.Canh();
+                        break;
+                    case 2:
+                        tuGiac tug = new tuGiac();
+                        tug.Canh();
+                        break;
+                    case 3:
+                        hinhVuong hinhVuong = new hinhVuong();
+                        hinhVuong.Canh();
+                        break;
+                    case 4:
+                        hinhBinhHanh hinhBinhHanh = new hinhBinhHanh();
+                        hinhBinhHanh.Canh();
+                        break;
+                    case 5:
+                        hinhChuNhat hinhChuNhat = new hinhChuNhat();
+                        hinhChuNhat.Canh();
+                        break;
+                    default:
+                        Console.WriteLine("Thoat!");
+                        return;
+                }
+            }
+            catch (Exception e) { }
+            
+
+        }
+    }
+    class daGiac
+    {
+        public virtual void Canh()
+        {
+            Console.WriteLine("so canh da giac : ");
+        }
+    }
+    class tamGiac : daGiac
+    {
+        public override void Canh()
+        {
+            Console.WriteLine("so canh tam giac : 3 canh");
+        }
+    }
+    class tuGiac : daGiac
+    {
+        public override void Canh()
+        {
+            Console.WriteLine("so canh tu giac : 4 canh");
+        }
+    }
+    class hinhVuong : daGiac
+    {
+        public override void Canh()
+        {
+            Console.WriteLine("so canh hinh vuong : 4 canh");
+        }
+    }
+    class hinhBinhHanh : daGiac
+    {
+        public override void Canh()
+        {
+            Console.WriteLine("so canh hinh binh Hanh : 4 canh");
+        }
+    }
+
+    class hinhChuNhat: daGiac
+    {
+        public override void Canh()
+        {
+            Console.WriteLine("so canh hinh chu nhat : 4 canh");
+        }
+    }
+}
